5475c5844d2f7a3e47848799f452e5e0ebb287d6
hawardShin <joushin@student.42seoul.kr>
2024-02-08 21:24:06 +0900

    Refactor: extract controller Method

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -12,18 +13,26 @@ public class MemberService {
 
     public long join(Member member){
         //같은 이름이 있는 중복회원 X
-        /*1. optional을 바로 반환하는 방법 별로 권장 X*/
-        Optional<Member> result = memberRepository.findByName(member.getName());
-//      Member mem = result.get(); //orElseGet 함수도 사용가능
-        result.ifPresent(m ->{//optional이기 때문에 할 수 있는 것
-            throw new IllegalStateException("이미 존재하는 회원입니다");
-        } );
-        /*2. 한번에 하는 방법*/
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m->{
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
-                });
-        memberRepository.save(member);
-        return member.getId();
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
     }
 }
