fd5b0e62d9e9c00535807b758fb4ed00eb14deb7
hawardShin <joushin@student.42seoul.kr>
2024-02-12 11:27:42 +0900

    Feat: Before Aop

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,4 @@
+package hello.hellospring.aop;
+
+public class TimeTraceAop {
+}
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -16,11 +16,21 @@ public class MemberService {
     }
 
     public long join(Member member){
+        long start = System.currentTimeMillis();
+
+    try{
         //같은 이름이 있는 중복회원 X
         validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
     }
+    finally {
+        long finish = System.currentTimeMillis();
+        long timeMs = finish - start;
+        System.out.println("join = " + timeMs + "ms");
+
+    }
+}
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
@@ -33,7 +43,14 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId){
